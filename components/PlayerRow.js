1f96cdc2e6fd3768439dcc2cdbe8535661f26dc2
bowingman <ngmaster.sag@gmail.com>
2019-08-20 18:55:06 -0400

    initial pass at roster

--- components/PlayerRow.js
+++ components/PlayerRow.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Image, Text, View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
-import { Ionicons } from '@expo/vector-icons';
 
 import FavoriteButton from './FavoriteButton';
 
@@ -29,23 +28,22 @@ const styles = StyleSheet.create({
   },
 });
 
-export default class TeamRow extends Component {
+export default class PlayerRow extends Component {
   constructor(props) {
     super(props);
+    if (this.props.showFavoriteIcon) {
+      this.state = {
+        showFavoriteIcon: this.props.showFavoriteIcon,
+      }
+      return;
+    }
+    // By default, don't show the favorite icon
+    this.state = {
+      showFavoriteIcon: false,
+    }
   }
 
   render() {
-    let icon = <View style={styles.iconPlacement}><Ionicons style={styles.icon} color='gray' size={36} name='ios-arrow-forward' /></View>
-    if (this.state.showFavoriteIcon) {
-      icon = (
-        <FavoriteButton
-          active={this.props.iconActive}
-          placementStyle={styles.iconPlacement}
-          iconStyle={styles.icon}
-          onFavorite={this.props.onFavorite}
-        />
-      )
-    }
     return (
         <View style={styles.row}>
           <Image
@@ -53,15 +51,19 @@ export default class TeamRow extends Component {
             source={{uri: this.props.logoURL}}
           />
           <Text style={styles.name}>{this.props.name}</Text>
-          {icon}
+          <FavoriteButton
+            active={this.props.iconActive}
+            placementStyle={styles.iconPlacement}
+            iconStyle={styles.icon}
+            onFavorite={this.props.onFavorite}
+          />
         </View>
     );
   }
 }
 
-TeamRow.propTypes = {
+PlayerRow.propTypes = {
   name: PropTypes.string,
   logoURL: PropTypes.string,
-  showFavoriteIcon: PropTypes.bool,
   onFavorite: PropTypes.func,
 };
